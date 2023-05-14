32669b60afb4fa825d61419f6f8649c61e139a5f
Kathryn6368 <6368@holbertonstudents.com>
2023-05-13 19:01:15 -0700

    handle malloc return

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,6 +13,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *node;
 
 	node = malloc(sizeof(*node));
+	if (node == NULL)
+	{
+		return (NULL);
+	}
 	node->n = value;
 	node->parent = parent;
 	node->left = NULL;
