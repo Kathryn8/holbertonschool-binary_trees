fed3c5c497912c88651f814ebd5d36fba0ccfc7a
cohort20 <sophia.jiyu@gmail.com>
2023-05-14 19:08:15 -0700

    update 9-binary_tree_height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,28 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - short description
- * @tree: a const binary_tree_t *data type variable
- *
- * Return: type is size_t
- */
-
+  * binary_tree_height - short description
+  * @tree: a binary_tree_t *data type variable
+  * Return: height of the tree
+  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left;
+	size_t right;
+
 	if (tree == NULL)
-	{
 		return (0);
-	}
 	if (tree->left == NULL && tree->right == NULL)
-	{
 		return (0);
-	}
-	if (binary_tree_height(tree->left) > binary_tree_height(tree->right))
-	{
-		return (1 + binary_tree_height(tree->left));
-	}
-	else
-	{
-		return (1 + binary_tree_height(tree->right));
-	}
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	return ((left > right) ? left + 1 : right + 1);
 }
